572c9f51db9469fdeeca56c0e2c82c32709559af
“Luis” <mrqueiroz22014@gmail.com>
2024-05-14 09:28:10 -0300

    Definindo Home page do Streamlit

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("MyGameHub: Sua plataforma definitiva de games")
+
+st.header("Bem-vindo ao MyGameHub!")
+st.write("Descubra, explore e compare os melhores jogos do momento. Fique por dentro das últimas novidades, análises e recomendações personalizadas para você.")
