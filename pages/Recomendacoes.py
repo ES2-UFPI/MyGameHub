b7f5b8276999def6e35f38dcb0b03c9731289d7e
“Luis” <mrqueiroz22014@gmail.com>
2024-07-09 08:49:20 -0300

    Close issue #14: Componente recuperador de informações desenvolvido

--- pages/Recomendacoes.py
+++ pages/Recomendacoes.py
@@ -1,55 +1,21 @@
+import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.agents import create_openai_functions_agent, AgentExecutor
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.memory import ChatMessageHistory
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chains import create_retrieval_chain
+
+@st.cache_resource
+def load_data():
+    df = pd.read_csv("src/data/processed_data.csv")
+    texts = df.apply(lambda x: x['title'] + " - " + x['game_description'], axis=1).tolist()
+    vectorstore = FAISS.from_texts(texts, embedding=OpenAIEmbeddings())
+    return vectorstore.as_retriever()
 
 load_dotenv()
-
-class RecommendationSystem:
-    def __init__(self):
-        self.llm = ChatOpenAI(
-            model='gpt-4o',
-            temperature=0.5
-        )
-        self.prompt = ChatPromptTemplate.from_messages([
-            ("system", "Você é um agente de inteligência artificial especializado em recomendar jogos da plataforma Steam. Quando o usuário digitar o nome de um jogo, você deve procurar por jogos bastante semelhantes e recomendá-los, fornecendo todas as informações possíveis dos jogos, como descrição, desenvolvedor, gênero, preço, data de lançamento e link. Responda com base na faixa de preço e data de lançamento especificada pelo usuário. Cordialmente, agradeça pela interação ao final."),
-            ("human", "{input}"),
-            MessagesPlaceholder(variable_name="agent_scratchpad")
-        ])
-        self.search = TavilySearchResults()
-        self.tools = [self.search]
-        self.agent = create_openai_functions_agent(
-            llm=self.llm,
-            prompt=self.prompt,
-            tools=self.tools
-        )
-        self.agent_executor = AgentExecutor(
-            agent=self.agent,
-            tools=self.tools
-        )
-
-    def get_recommendations(self, user_input):
-        response = self.agent_executor.invoke({
-            "input": user_input
-        })
-        return response["output"]
-
-agent = RecommendationSystem()
-
-st.title("Sistema de Recomendações de Jogos")
-
-game_name = st.text_input("Digite o nome de um jogo para obter recomendações:")
-
-release_date_range = st.slider("Selecione a faixa de data de lançamento", 1980, 2024, (1980, 2024))
-
-if st.button("Buscar Recomendações"):
-    if game_name:
-        with st.spinner('Buscando recomendações...'):
-
-            user_input = f"Nome do jogo: {game_name}. Data de lançamento: entre {release_date_range[0]} e {release_date_range[1]}."
-            response = agent.get_recommendations(user_input)
-            st.write(response)
-    else:
-        st.error("Por favor, digite o nome de um jogo.")
+retriever = load_data()
