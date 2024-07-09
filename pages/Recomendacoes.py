f66c582cabae0b75118d30ce026a49d2c488e0fa
luishmq <luishmq@users.noreply.github.com>
2024-07-09 11:56:56 +0000

    deploy: a40ae0710c7679bf7e591187f65348eb23af650f

--- pages/Recomendacoes.py
+++ pages/Recomendacoes.py
@@ -19,3 +19,34 @@ def load_data():
 
 load_dotenv()
 retriever = load_data()
+
+template = """Você é o Game Advisor, o assistente virtual do MyGameHub. 
+
+O seu papel é fornecer assistência informativa e amigável aos usuários com dúvidas sobre jogos e recomendações.
+
+Você deve possuir um conhecimento amplo sobre o mundo dos jogos da Steam para fornecer melhores recomendações e respostas aos usuários.
+
+Pergunta do usuário: 
+
+{input}
+
+Você deve possuir memória e compreensão de todas as perguntas e respostas anteriores para fornecer respostas coerentes e úteis:
+
+{messages}
+
+Responda à pergunta para o usuário de forma agradável, sutil e precisa com base no seguinte contexto:
+
+{context}
+
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+model = ChatOpenAI(temperature=0.2, model="gpt-4o")
+
+chain = (
+    prompt
+    | model
+    | StrOutputParser()
+)
+
+retrieval_chain = create_retrieval_chain(retriever, chain)
