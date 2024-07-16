bbccc234e834aca4e0e19bfc73b41e246e8c9265
Vlaady <Vlaady@users.noreply.github.com>
2024-07-16 13:00:34 +0000

    deploy: 04929b34db9dcc5a1fe968a16a4ec1def318a845

--- pages/Journal.py
+++ pages/Journal.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from news import news_data
 
-# Configuração da página
 st.set_page_config(page_title="Jornal de Jogos", page_icon=":newspaper:", layout="wide")
 
-# Centralizar o título
+
 st.markdown(
     """
     <style>
@@ -15,25 +14,47 @@ st.markdown(
         height: 100px;
         font-size: 3em;
         font-weight: bold;
+        font-family: 'Inter Var', sans-serif;
+        text-rendering: optimizeLegibility;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    .link {
+        color: #FFD700;
     }
     </style>
     <div class="title">Journal</div>
     """,
     unsafe_allow_html=True
-)
-
-# Seção de Notícias
-st.header("Notícias Recentes")
-for article in news_data:
-    col1, col2 = st.columns([1, 5])
-    with col1:
-        st.image(article["image"], width=100)
-    with col2:
-        st.markdown(f"### [{article['title']}](?article_id={article['id']})")
-        st.write(article["excerpt"])
-        st.markdown(f"<small>{article['time']} - {article['comments']} comentários</small>", unsafe_allow_html=True)
-    st.markdown("---")
+) 
 
 # Obter o ID do artigo da URL
-article_id = st.experimental_get_query_params().get("article_id", [None])[0]
+query_params = st.experimental_get_query_params()
+article_id = query_params.get("article_id", [None])[0]
 
+if article_id:
+    article = next((item for item in news_data if item["id"] == article_id), None)
+    if article:
+        st.title(article["title"])
+        if "subtitle" in article:
+            st.subheader(article["subtitle"])
+        st.write(article["content"])
+    else:
+        st.write("Notícia não encontrada.")
+else:
+    # Seção de Notícias
+    st.header("Notícias Recentes")
+    for article in news_data:
+        col1, col2 = st.columns([1, 5])
+        with col1:
+            if "image" in article:
+                st.image(article["image"], width=240)  # Aumentar o tamanho da imagem
+        with col2:
+            st.markdown(f"### <a href='?article_id={article['id']}' class='link'>{article['title']}</a>", unsafe_allow_html=True)
+            st.write(article["excerpt"])
+        st.markdown("---")
