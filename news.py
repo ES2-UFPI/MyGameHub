d32b65d87d539f728af2845770eb416b2c05e67a
Vlaady <wladimirgadelha@ufpi.edu.br>
2024-07-18 10:06:20 -0300

    Close issue#24

--- news.py
+++ news.py
@@ -1,26 +1,43 @@
 news_data = [
+    
+    {
+        "id": "5",
+        "title": "Após 9 anos do lançamento, jogadores descobrem cena cortada do final de The Witcher 3",
+        "excerpt": "Um YouTuber conseguiu restaurar uma cena final que não estava presente na versão final do jogo.",
+        "content": "",
+        "image": "images/News_5.webp", 
+    },
+    
+    {
+        "id": "4",
+        "title": "MMO em desenvolvimento há 11 anos finalmente está pronto para ser lançado e marca data para acesso antecipado",
+        "excerpt": "Mais uma década para o lançamento oficial?",
+        "content": "",
+        "image": "images/News_4.webp", 
+    },
+         
     {
         "id": "3",
         "title": "Inglaterra ou Espanha? EAFC nos mostra quem será o campeão da Euro 2024",
-
-        "excerpt": "Simulamos a final do torneio europeu.",
-        "content": "Conteúdo completo da notícia sobre FIFA",
+        "excerpt": "Todos querem saber qual será a seleção campeã da Euro 2024: Espanha ou Inglaterra? E nós podemos te ajudar a descobrir quem será a vencedora na disputa europeia.",
+        "content": "",
         "image": "images/News_3.webp", 
     },
     {
         "id": "2",
         "title": "'Não somos suas empregadas', criadoras de mod de Baldur's Gate 3 para Stardew Valley sofrem ataques de fãs e rebatem",
-      
         "excerpt": "Três desenvolvedoras estão trabalhando em um mod que traz personagens do RPG da Larian Studios para o indie da ConcernedApe. Mas tem muita gente impaciente que está reclamando e até atacando o trio.",
-        "content": "Conteúdo completo da notícia sobre Stardew Valley",
+        "content": "",
         "image": "images/News_2.webp", 
     },
+    
     {
         "id": "1",
         "title": "A vilã não é tão vilã: Elden Ring: Shadow of the Erdtree conta o passado da Rainha Marika e ela não é tão má assim",
-        
-        "excerpt": "O passado da vilã.",
-        "content": "Como de costume, a FromSoftware deixou para responder algumas questões e contar certas narrativas da trama de Elden Ring no DLC Shadow of the Erdtree. Ao seguirmos os passos de Miquella no Reino das Sombras conhecemos novas localidades, inimigos mortais e novas histórias peculiares e até mesmo um pouco assustadoras. Dentro deste reino, podemos conhecer o passado da Rainha Marika, a manipuladora personagem que é de suma importância para os eventos que desencadearam a Ruptura — momento de quebra do Anel Prístino — nas Terras Intermédias. Apesar de ser malevolente, descobrimos que a criadora da Ordem Dourada, não é tão má, nem tão vilã em Shadow of the Erdtree, porque tomamos conhecimento de seu passado bastante sofrido.",
+        "excerpt": "Rituais de Jarro são malignos e podem ter despertado a personalidade manipuladora e maligna de Marika.",
+        "content": "",
         "image": "images/News_1.webp", 
     },
+    
+
 ]
