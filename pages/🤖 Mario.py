7f160bee6d3b32eac562df070f450c12c42fadf5
“Luis” <mrqueiroz22014@gmail.com>
2024-07-21 11:48:14 -0300

    Adicionando secrets

--- pages/🤖 Mario.py
+++ pages/🤖 Mario.py
@@ -50,7 +50,7 @@ class Mario:
         return ChatPromptTemplate.from_template(template)
 
     def _initialize_model(self):
-        return ChatOpenAI(temperature=0.2, model="gpt-4o")
+        return ChatOpenAI(temperature=0.2, model="gpt-4o", api_key=st.secrets["openai"]["api_key"])
 
     def _create_chain(self):
         return (
