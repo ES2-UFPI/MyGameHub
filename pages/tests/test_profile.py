e24f000be5925b8491b2d3eeed068db37ad501a8
milio <emiliods79@gmail.com>
2024-08-06 09:04:29 -0300

    closeissue#63

--- pages/tests/test_profile.py
+++
@@ -1,5 +0,0 @@
-from streamlit.testing.v1 import AppTest
-
-def test_profile():
-    at = AppTest.from_file("../Profile.py").run(timeout=50)
-    assert not at.error, "Erro encontrado durante a execução inicial"
